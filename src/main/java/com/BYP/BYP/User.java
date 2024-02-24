b59cd90de49eb09e6e045e2c4c593757d1898026
Federallo <aligiassioma72@gmail.com>
2024-02-24 14:40:56 +0100

    created form for adding users in the database

--- src/main/java/com/BYP/BYP/User.java
+++ src/main/java/com/BYP/BYP/User.java
@@ -27,17 +27,33 @@ public class User {
   public Integer getId() {
     return this.id;
   }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
   
   public String getUsername() {
     return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
   }
 
   public String getEmail() {
     return this.email;
   }
 
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   public Boolean getIsSuperUser() {
     return this.isSuperUser;
+  }
+
+  public void setIsSuperUser(Boolean isSuperUser) {
+    this.isSuperUser = isSuperUser;
   }
 
 }
--- src/main/java/com/BYP/BYP/UserController.java
+++ src/main/java/com/BYP/BYP/UserController.java
@@ -2,23 +2,37 @@ package com.BYP.BYP;
 
 import jakarta.persistence.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class UserController {
 
   private final UserRepository userRepository;
 
+  @Autowired // for automatic dependency injection (reduces the need for getters and setters)
   public UserController(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
-  @GetMapping("/users")
+  /*@GetMapping("/users")
   public Iterable<User> findAllUsers() {
     return this.userRepository.findAll();
+  }*/
+
+  // creating a new user using a form
+  @RequestMapping(value = "/addUser", method = RequestMethod.POST) // note: value represents the URL path
+  public String addUser(@ModelAttribute User user) {
+    
+	//save the user into the database
+	userRepository.save(user);
+
+	//return the page where it says "User added successfully"
+    	return "userAdded.html"; // this is only a temporary solution
   }
 
-  @PostMapping("/users")
-  public User addOneEmployee(@RequestBody User user) {
-    return this.userRepository.save(user);
+  @RequestMapping("/addUserForm")
+  public String addUserForm() {
+    return "addUser.html";
   }
 }
