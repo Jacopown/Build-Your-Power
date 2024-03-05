fb48ccae4474f9469c3de0b3706873b94e54a76b
Federallo <aligiassioma72@gmail.com>
2024-03-05 17:43:50 +0100

    created random behaviour for batteries' status

---
+++ src/main/java/com/BYP/BYP/BatteryService.java
@@ -0,0 +1,61 @@
+package com.BYP.BYP;
+
+import java.util.Random;
+
+import com.BYP.entity.Battery;
+import com.BYP.DAO.BatteryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BatteryService {
+
+	//TODO add messaging template for convert and send later
+	private final BatteryRepository batteryRepository;
+
+	private final Random random = new Random();
+
+	@Autowired
+	public BatteryService(BatteryRepository batteryRepository) {
+		this.batteryRepository = batteryRepository;
+	}
+
+	// creating random behaviour for batteries
+	public void performRandomActionsWithDelay(Battery battery, int maxIterations, Long delay) { // delay is a value expressed in milliseconds
+		for (int i = 0; i < maxIterations; i++) {
+			performRandomAction(battery);
+			sleep(delay);
+		}
+	}
+
+	private void performRandomAction(Battery battery) {
+
+		int amountOfStatuses = 3;
+		int randomAction = random.nextInt(amountOfStatuses);
+
+		//TODO must be added messaging system (convert and send)
+		switch(randomAction) {
+			case 0:
+				batteryRepository.update(battery, new String[]{"AVALIABLE"});
+				break;
+			case 1:
+				batteryRepository.update(battery, new String[]{"UNAVALIABLE"});
+				break;
+			case 2:
+				batteryRepository.update(battery, new String[]{"DAMAGED"});
+				break;
+			case 3:
+				batteryRepository.update(battery, new String[]{"UNREACHABLE"});
+				break;
+		}
+	}
+
+	private void sleep(Long delay) {
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
