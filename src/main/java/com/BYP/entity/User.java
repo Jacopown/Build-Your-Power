b8193744d2ed8e968f8b2f1638136d61aafac6e8
Jacopown <jacopomehi@gmail.com>
2024-03-11 18:25:07 +0100

    made id Long type and added some User attributes

--- src/main/java/com/BYP/entity/User.java
+++ src/main/java/com/BYP/entity/User.java
@@ -7,10 +7,24 @@ import jakarta.persistence.*;
 public class User {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
-  private String username;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true, length = 45)
   private String email;
+
+  @Column(nullable = false, unique = true, length = 20)
+  private String username;
+
+  @Column(nullable = false, length = 64)
+  private String password;
+  
+  @Column(nullable = false, length = 20)
+  private String firstName;
+
+  @Column(nullable = false, length = 20)
+  private String lastName;
+
   private Boolean isSuperUser;
   
   
@@ -18,28 +32,20 @@ public class User {
   // though it does not necessarily have to be public.
   private User() {}
   
-  public User(String username, String email, Boolean isSuperUser) {
+  public User(String username, String email, String lastName, Boolean isSuperUser) {
     this.username = username;
     this.email = email;
     this.isSuperUser = isSuperUser;
   }
 
-  public Integer getId() {
+  public Long getId() {
     return this.id;
   }
 
-  public void setId(Integer id) {
+  public void setId(Long id) {
     this.id = id;
   }
   
-  public String getUsername() {
-    return this.username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
   public String getEmail() {
     return this.email;
   }
@@ -48,6 +54,37 @@ public class User {
     this.email = email;
   }
 
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return this.username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String lastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
   public Boolean getIsSuperUser() {
     return this.isSuperUser;
   }
