a589d77479e341af19c68765830ef44192d76324
Jacopown <jacopomehi@gmail.com>
2024-03-13 14:43:09 +0100

    first attempt to make user able to register himself

--- src/main/java/com/BYP/controller/HomePageController.java
+++ src/main/java/com/BYP/controller/HomePageController.java
@@ -1,20 +1,39 @@
-package com.BYP.BYP;
+package com.BYP.controller;
 
+import com.BYP.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.BYP.DAO.UserRepository;
 
 @Controller
 public class HomePageController {
 
+  @Autowired // for automatic dependency injection (reduces the need for getters and setters)
+  public UserRepository userRepository;
+
 	@Value("${spring.application.name}")
 	String appName;
 
-	@GetMapping("/")
+	@GetMapping("")
 	public String homePage(Model model) {
 		model.addAttribute("appName", appName);
 		return "home";
 	}
+
+	@GetMapping("/register")
+	public String showSignUpForm(Model model) {
+		// model.addAttribute("user", new User());
+		return "signup_form";
+	}
+
+  @PostMapping("/signup")
+  public String submitSignUpForm(User user) {
+    userRepository.save(user);
+    return "registration_success";
+  }
 }
