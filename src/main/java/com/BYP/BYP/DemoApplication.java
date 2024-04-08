03fdbcb54ec67cbff5c60538040ae808bbd9008f
Jacopown <jacopomehi@gmail.com>
2024-04-08 10:40:26 +0200

    renamed entity to model and commented out temporary unused parts

--- src/main/java/com/BYP/BYP/DemoApplication.java
+++ src/main/java/com/BYP/BYP/DemoApplication.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.BYP.DAO.BatteryRepository;
 import com.BYP.DAO.StationRepository;
-import com.BYP.entity.Battery;
-import com.BYP.entity.Station;
+import com.BYP.model.Battery;
+import com.BYP.model.Station;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,7 +18,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.BYP")
-@EntityScan("com.BYP.entity")
+@EntityScan("com.BYP.model")
 @EnableJpaRepositories("com.BYP.repository")
 public class DemoApplication {
 
@@ -30,31 +30,31 @@ public class DemoApplication {
   }
 
   //creating automatic entities
-  @Bean
-  public CommandLineRunner demo(StationRepository stationRepository, BatteryRepository batteryRepository) {
-	  return(args) -> {
-		  //creating stations
-		  Station station1 = new Station("First Floor");
-		  Station station2 = new Station("Second Floor");
-		  stationRepository.save(station1);
-		  stationRepository.save(station2);
-
-		  //creating batteries
-		  Battery battery1 = new Battery(Battery.BatteryStatus.AVAILABLE, station1, 12.0f, 25.0f);
-		  Battery battery2 = new Battery(Battery.BatteryStatus.UNAVAILABLE, station1, 12.0f, 25.0f);
-		  Battery battery3 = new Battery(Battery.BatteryStatus.DAMAGED, station2, 12.0f, 25.0f);
-		  batteryRepository.save(battery1);
-		  batteryRepository.save(battery2);
-		  batteryRepository.save(battery3);
-
-		  //creating random behaviour for batteries
-		  int numberOfActions = 5; // represent the number of times the single battery will change its status
-		  long delay = 10000; // which is 10 seconds
-		  BatteryService batteryService = new BatteryService(batteryRepository, simpMessagingTemplate);
-		  batteryService.performRandomActionsWithDelay(battery1, numberOfActions, delay);
-		  batteryService.performRandomActionsWithDelay(battery2, numberOfActions, delay);
-		  batteryService.performRandomActionsWithDelay(battery3, numberOfActions, delay);
-	 };
-  }
+  // @Bean
+  // public CommandLineRunner demo(StationRepository stationRepository, BatteryRepository batteryRepository) {
+	 //  return(args) -> {
+		//   //creating stations
+		//   Station station1 = new Station("First Floor");
+		//   Station station2 = new Station("Second Floor");
+		//   stationRepository.save(station1);
+		//   stationRepository.save(station2);
+  //
+		//   //creating batteries
+		//   Battery battery1 = new Battery(Battery.BatteryStatus.AVAILABLE, station1, 12.0f, 25.0f);
+		//   Battery battery2 = new Battery(Battery.BatteryStatus.UNAVAILABLE, station1, 12.0f, 25.0f);
+		//   Battery battery3 = new Battery(Battery.BatteryStatus.DAMAGED, station2, 12.0f, 25.0f);
+		//   batteryRepository.save(battery1);
+		//   batteryRepository.save(battery2);
+		//   batteryRepository.save(battery3);
+  //
+		//   //creating random behaviour for batteries
+		//   int numberOfActions = 5; // represent the number of times the single battery will change its status
+		//   long delay = 10000; // which is 10 seconds
+		//   BatteryService batteryService = new BatteryService(batteryRepository, simpMessagingTemplate);
+		//   batteryService.performRandomActionsWithDelay(battery1, numberOfActions, delay);
+		//   batteryService.performRandomActionsWithDelay(battery2, numberOfActions, delay);
+		//   batteryService.performRandomActionsWithDelay(battery3, numberOfActions, delay);
+	 // };
+  // }
 
 }
