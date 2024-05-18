68885c4a6ea5973c50b516537201acc1a211ffa0
Federallo <aligiassioma72@gmail.com>
2024-05-18 13:42:22 +0200

    fixed missing @Service

--- src/main/java/com/BYP/CustomUserDetailsService.java
+++ src/main/java/com/BYP/CustomUserDetailsService.java
@@ -4,15 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 import com.BYP.DAO.UserRepository;
 import com.BYP.model.User;
 import com.BYP.model.CustomUserDetails;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
  
     @Autowired
     private UserRepository userRepo;
-
+	
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByEmail(username).get();
