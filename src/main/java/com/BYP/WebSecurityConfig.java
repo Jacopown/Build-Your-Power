76432121702d97f96f4efab1ae2d8b8c3399c469
Federallo <aligiassioma72@gmail.com>
2024-05-22 16:34:14 +0200

    fixed register_form webpage not displaying

--- src/main/java/com/BYP/WebSecurityConfig.java
+++ src/main/java/com/BYP/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig {
         http
             .authenticationProvider(authenticationProvider())
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/login", "/register", "/userView").permitAll()
+                .requestMatchers("/login", "/signup", "/userView").permitAll()
                 //.requestMatchers("/userView").hasRole("USER") // Adjust roles as needed
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
